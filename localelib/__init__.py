50ee7611271daf7c39197c44f1d474b1a3052339
RandomInternetUser <83414705+jailedfish@users.noreply.github.com>
2026-01-24 23:37:32 +0300

    new version

--- localelib/__init__.py
+++ localelib/__init__.py
@@ -1,45 +1,35 @@
 import os
 
-class Locale:
-    code: str
+class Message:
+    name: str
+    locales: dict[str, str]
+    def __init__(self, name:str, locales: dict[str, str]):
+        self.locales = locales
+        self.name = name
+    
+    def localed(self, locale_id: str):
+        return self.locales[locale_id]
 
-    def __init__(self, code: str, dictionary: dict[str, str]):
-        self.code = code
-        for arg, val in dictionary.items():
-            setattr(self, arg, val)
-
-    def get(self, word: str, default: str = ''):
-        try:
-            return eval(f'self.{word}', {'self': self})
-        except AttributeError:
-            return default
-
-    def add(self, word: str, locale: str) -> None:
-        setattr(self, word, locale)
-
-    def __getitem__(self, item):
-        try:
-            return eval(f'self.{item}', {'self': self})
-        except NameError:
-            return f'{item} (translation missing)'
+    def add(self, code: str, locale: str) -> None:
+        self.locales[code] = locale
+        
 
 
 class LocaleManager:
-    locales: list[Locale] = []
-
+    messages: list[Message] = []
+    
     def __init__(self, locale_folder: str):
         if not os.path.exists(locale_folder):
             raise FileNotFoundError(f'locale folder {locale_folder} not found')
         self.locales_folder = locale_folder
-
+    
     def setup(self):
         if self.locales_folder:
-            import os
             for filename in os.listdir(self.locales_folder):
                 if not filename.startswith('locale_') and not filename.endswith('.lc'):
                     continue
+                    
                 with open(os.path.join(self.locales_folder, filename), 'r', encoding='utf-8') as file:
-                    locale_dict = {}
                     lines = file.read().split('\n')
                     code = filename[filename.rfind('_') + 1: filename.rfind('.')]
                     for entry in lines:
@@ -48,13 +38,18 @@ class LocaleManager:
 
                         key, val = entry.split(': ')
                         val = val.replace(':/ ', ': ').replace('\\n', '\n')
-                        locale_dict[key] = val
+                        for i, msg in enumerate(self.messages):
+                            if msg.name == key:
+                                self.messages[i].add(code, val)
+                                break
+                        else:
+                            continue
+                        self.messages.append(Message(key, {code: val}))
 
-                    self.locales.append(Locale(code, locale_dict))
             return self
 
-    def get(self, code: str) -> Locale:
-        for locale in self.locales:
-            if locale.code == code:
-                return locale
+    def get(self, name: str) -> Message:
+        for message in self.messages:
+            if message.name == name:
+                return message
         raise KeyError
